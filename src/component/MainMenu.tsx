0655dd54b5f8a543e5edb2ab2fa9db35fc7d07c1
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-22 10:14:47 +0700

    add peraturan url

--- src/component/MainMenu.tsx
+++ src/component/MainMenu.tsx
@@ -5,29 +5,29 @@ import { useBrandingContext } from '@/context/BrandingContext'; // Adjust the pa
 import LinkItem from "@/component/LinkItem";
 
 export default function MainMenu() {
-  const { perencanaanUrl, realisasiUrl, tppUrl, laporanUrl, manriskUrl } = useUrlContext();
-  const { logo } = useBrandingContext();
+    const { perencanaanUrl, realisasiUrl, tppUrl, laporanUrl, manriskUrl, peraturanUrl } = useUrlContext();
+    const { logo } = useBrandingContext();
 
-  // default logo
-  const logoSrc = logo;
+    // default logo
+    const logoSrc = logo;
 
-  const linksMenu = [
-    { href: perencanaanUrl, title: "Perencanaan", imgSrc: logoSrc, enabled: true },
-    { href: realisasiUrl, title: "Realisasi", imgSrc: logoSrc, enabled: true },
-    { href: laporanUrl, title: "Laporan", imgSrc: logoSrc, enabled: true },
-    { href: tppUrl, title: "TPP", imgSrc: logoSrc, enabled: true },
-  ];
+    const linksMenu = [
+        { href: perencanaanUrl, title: "Perencanaan", imgSrc: logoSrc, enabled: true },
+        { href: realisasiUrl, title: "Realisasi", imgSrc: logoSrc, enabled: true },
+        { href: laporanUrl, title: "Laporan", imgSrc: logoSrc, enabled: true },
+        { href: peraturanUrl, title: "Peraturan Perundang-undangan, dan Dokumen Lainnya", imgSrc: logoSrc, enabled: true },
+    ];
 
-  return (
-    <main className="flex flex-col items-center justify-center gap-10 px-15 py-5 mt-10">
-      {linksMenu.filter(link => link.enabled).map((link, index) => (
-        <LinkItem
-          key={index}
-          href={link.href}
-          title={link.title}
-          imgSrc={link.imgSrc}
-        />
-      ))}
-    </main >
-  )
+    return (
+        <main className="flex flex-col items-center justify-center gap-10 px-15 py-5 mt-10">
+            {linksMenu.filter(link => link.enabled).map((link, index) => (
+                <LinkItem
+                    key={index}
+                    href={link.href}
+                    title={link.title}
+                    imgSrc={link.imgSrc}
+                />
+            ))}
+        </main >
+    )
 }
--- src/context/UrlContext.tsx
+++ src/context/UrlContext.tsx
@@ -3,12 +3,13 @@
 import { createContext, useContext } from "react"
 
 interface UrlContextType {
-  authUrl: string;
-  perencanaanUrl: string;
-  realisasiUrl: string;
-  tppUrl: string;
-  laporanUrl: string;
-  manriskUrl: string;
+    authUrl: string;
+    perencanaanUrl: string;
+    realisasiUrl: string;
+    tppUrl: string;
+    laporanUrl: string;
+    manriskUrl: string;
+    peraturanUrl: string;
 }
 
 const AUTH_URL = process.env.NEXT_PUBLIC_AUTH_URL || "#";
@@ -17,22 +18,23 @@ const REALISASI_URL = process.env.NEXT_PUBLIC_REALISASI || "#";
 const TPP_URL = process.env.NEXT_PUBLIC_TPP || "#";
 const LAPORAN_URL = process.env.NEXT_PUBLIC_LAPORAN || "#";
 const MANRISK_URL = process.env.NEXT_PUBLIC_MANRISK || "#";
+const PERATURAN_URL = process.env.NEXT_PUBLIC_PERATURAN_URL || "#";
 
 // context
 const UrlContext = createContext<UrlContextType | undefined>(undefined);
 
 export function UrlProvider({ children }: Readonly<{ children: React.ReactNode; }>) {
-  return (
-    <UrlContext.Provider value={{ authUrl: AUTH_URL, perencanaanUrl: PERENCANAAN_URL, realisasiUrl: REALISASI_URL, tppUrl: TPP_URL, laporanUrl: LAPORAN_URL, manriskUrl: MANRISK_URL }}>
-      {children}
-    </UrlContext.Provider>
-  );
+    return (
+        <UrlContext.Provider value={{ authUrl: AUTH_URL, perencanaanUrl: PERENCANAAN_URL, realisasiUrl: REALISASI_URL, tppUrl: TPP_URL, laporanUrl: LAPORAN_URL, manriskUrl: MANRISK_URL, peraturanUrl: PERATURAN_URL }}>
+            {children}
+        </UrlContext.Provider>
+    );
 }
 
 export function useUrlContext() {
-  const context = useContext(UrlContext);
-  if (context === undefined) {
-    throw new Error("useUrlContext must be used witihin a UrlProvider")
-  }
-  return context;
+    const context = useContext(UrlContext);
+    if (context === undefined) {
+        throw new Error("useUrlContext must be used witihin a UrlProvider")
+    }
+    return context;
 }
